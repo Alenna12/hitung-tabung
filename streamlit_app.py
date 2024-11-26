00e943ec5f2b5c4a1f6f9836d56521244031d89b
alenna_ <valenviarozii@gmail.com>
2024-11-26 16:31:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("Menghitung volume :red[Volume Tabung] :rocket:")
 
+r = st.number_input("Masukan Jari-Jari (cm) ",0)
+t = st.number_input("Masukan tinggi (cm) ",0)
